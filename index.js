9e2e9a9a7c22ae9538c17e6ac4462c5a64899d29
joshlacey <williamjoshualacey@gmail.com>
2017-08-28 20:06:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  let counter = 0
+  function addBump() { counter += 1};
+  function getBumps() { return counter };
+  return {addBump, getBumps};
+}
+
+function createAnimal(animal) {
+  function something(device) {
+    return {animalType: animal, deadlyDevice: device};
+  }
+  return something;
+}
+
+let sharkCreator = createAnimal('Shark');
+let sharkWithFrickinCannon = sharkCreator('Cannon');
+
+let sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
